c262798b01f37b18d68e8117e680fc61af5b4ee8
Alex Markessinis <markea@almyz125.com>
2016-04-22 13:54:20 -0400

    commented routes, and protected dashboard and timeline

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,48 +11,58 @@
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-
-    Route::get('/', function () {
-        return redirect()->route('home');
-    });
-
-    Route::get('/home', function () {
-        return redirect()->route('home');
-    });
-
-    Route::auth();
-
-    Route::group(['middleware' => ['admin'], 'prefix' => 'users'], function () {
-
-        Route::get('/', ['as' => 'users', 'uses' => 'HomeController@userIndex']);
-
-        Route::get('/new', ['as' => 'newuser', 'uses' => 'HomeController@showNewUserForm']);
-        Route::post('/new', ['as' => 'newuser', 'uses' => 'HomeController@postNewUser']);
-
-        Route::group(['prefix' => '{id}'], function () {
-            Route::get('/', 'HomeController@getUser');
-            Route::get('/del', 'HomeController@deleteUser');
-            Route::get('/trash', 'HomeController@viewTrashedUser');
-            Route::post('/trash', 'HomeController@restoreTrashedUser');
-            Route::post('/save', 'HomeController@saveUser');
-        });
-
-    });
-
-    Route::group(['prefix' => 'verify'], function () {
-        Route::get('/', ['as' => 'home', 'uses' => 'VerificationController@index']);
-        Route::post('/', ['as' => 'home', 'uses' => 'VerificationController@verify']);
-        Route::get('/failure', ['as' => 'verify_fail', 'uses' => 'VerificationController@verifyFail']);
-        Route::get('/success', ['as' => 'verify_success', 'uses' => 'VerificationController@verifySuccess']);
-    });
-
+// Redirect to the main page
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+// Redirect to the main page
+Route::get('/home', function () {
+    return redirect()->route('home');
+});
+// Enable the auth routes (login, logout, ect..)
+Route::auth();
+// A user's profile page
+Route::get('profile', ['as' => 'profile', 'uses' => 'HomeController@profile']);
+// Save any changes made to a user's profile
+Route::post('profile', ['as' => 'profile', 'uses' => 'HomeController@saveProfile']);
+//
+Route::get('passwd/change', ['as' => 'change_passwd', 'uses' => 'HomeController@changePassword']);
+// Routes that only administrators can visit.
+Route::group(['middleware' => ['admin']], function () {
+    // Analytic routes
     Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
-
-    Route::get('profile', ['as' => 'profile', 'uses' => 'HomeController@profile']);
-
-    Route::post('profile', ['as' => 'profile', 'uses' => 'HomeController@saveProfile']);
-
-    Route::get('passwd/change', ['as' => 'change_passwd', 'uses' => 'HomeController@changePassword']);
-
+    Route::get('timeline', ['as' => 'dashboard', 'uses' => 'HomeController@timeline']);
+    // Local user management routes
+    Route::group(['prefix' => 'users'], function () {
+        // View all users
+        Route::get('/', ['as' => 'users', 'uses' => 'HomeController@userIndex']);
+        // User creation form
+        Route::get('/new', ['as' => 'newuser', 'uses' => 'HomeController@showNewUserForm']);
+        // Post user creation form
+        Route::post('/new', ['as' => 'newuser', 'uses' => 'HomeController@postNewUser']);
+        // All routes that pertain to a specific user
+        Route::group(['prefix' => '{id}'], function () {
+            // View a user's info
+            Route::get('/', 'HomeController@getUser');
+            // Save any chnages made to a user's info
+            Route::post('/save', 'HomeController@saveUser');
+            // Delete a user
+            Route::get('/del', 'HomeController@deleteUser');
+            // View a user in the trash
+            Route::get('/trash', 'HomeController@viewTrashedUser');
+            // Restore the user from the trash
+            Route::post('/trash', 'HomeController@restoreTrashedUser');
+        });
+    });
+});
+// Routes that deal with verifying external users
+Route::group(['prefix' => 'verify'], function () {
+    // The main page
+    Route::get('/', ['as' => 'home', 'uses' => 'VerificationController@index']);
+    // Send verification form
+    Route::post('/', ['as' => 'home', 'uses' => 'VerificationController@verify']);
+    // The verification request was successful, show the success page
+    Route::get('/success', ['as' => 'verify_success', 'uses' => 'VerificationController@verifySuccess']);
+    // The verification request was unsuccessful, show the failure page
+    Route::get('/failure', ['as' => 'verify_fail', 'uses' => 'VerificationController@verifyFail']);
 });
